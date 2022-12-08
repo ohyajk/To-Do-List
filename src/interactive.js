e0c535e368d5c97fd10c7535504ad3a2bd816f2c
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2022-12-09 00:50:24 +0530

    Fix linter errors

--- src/interactive.js
+++ src/interactive.js
@@ -1,30 +1,24 @@
 const cbs = document.querySelectorAll('#cb');
-let tlGet = JSON.parse(localStorage.getItem('taskList'));    
-
+let tlGet = JSON.parse(localStorage.getItem('taskList'));
 
 cbs.forEach((cb, index) => {
-    cb.addEventListener('change', (e) => {
-        if(e.target.checked){
-            tlGet[index].completed = true;
-            localStorage.setItem('taskList', JSON.stringify(tlGet));
-        }else {
-            tlGet[index].completed = false;
-            localStorage.setItem('taskList', JSON.stringify(tlGet));
+  cb.addEventListener('change', (e) => {
+    if (e.target.checked) {
+      tlGet[index].completed = true;
+      localStorage.setItem('taskList', JSON.stringify(tlGet));
+    } else {
+      tlGet[index].completed = false;
+      localStorage.setItem('taskList', JSON.stringify(tlGet));
+    }
+  });
+});
 
-        }
-    })
-})
+const clearAll = document.querySelector('#clearAll');
 
-const clearAll = document.querySelector('#clearAll')
-
-tlGet.forEach((t, index) => {
-    clearAll.addEventListener('click', ()=> {
-        tlGet = tlGet.filter(t => {
-            return t.completed == false; 
-        });
-        localStorage.setItem('taskList', JSON.stringify(tlGet));
-        window.location.reload();
-    })
-})
-
-
+tlGet.forEach(() => {
+  clearAll.addEventListener('click', () => {
+    tlGet = tlGet.filter((t) => t.completed == false);// eslint-disable-line
+    localStorage.setItem('taskList', JSON.stringify(tlGet));
+    window.location.reload();
+  });
+});
