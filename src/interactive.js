875cbe37883f2195331fd082782e904b8de720ae
Jitender Kumar <114587346+ohyajk@users.noreply.github.com>
2022-12-09 00:41:02 +0530

    Add interactivity js

---
+++ src/interactive.js
@@ -0,0 +1,30 @@
+const cbs = document.querySelectorAll('#cb');
+let tlGet = JSON.parse(localStorage.getItem('taskList'));    
+
+
+cbs.forEach((cb, index) => {
+    cb.addEventListener('change', (e) => {
+        if(e.target.checked){
+            tlGet[index].completed = true;
+            localStorage.setItem('taskList', JSON.stringify(tlGet));
+        }else {
+            tlGet[index].completed = false;
+            localStorage.setItem('taskList', JSON.stringify(tlGet));
+
+        }
+    })
+})
+
+const clearAll = document.querySelector('#clearAll')
+
+tlGet.forEach((t, index) => {
+    clearAll.addEventListener('click', ()=> {
+        tlGet = tlGet.filter(t => {
+            return t.completed == false; 
+        });
+        localStorage.setItem('taskList', JSON.stringify(tlGet));
+        window.location.reload();
+    })
+})
+
+
